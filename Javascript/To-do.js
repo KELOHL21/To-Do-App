27314607e1ea63880f7315ea5e059affa575ac34
Kelsy <kelsyohlsson@gmail.com>
2022-04-12 17:07:18 +0200

    Determining the Javascript

--- Javascript/To-do.js
+++ Javascript/To-do.js
@@ -1,20 +1,21 @@
-let addtaskbutton = document.getElementById('submitbtn');
+/*let addtaskbutton = document.getElementById('submitbtn');
 let task_el = document.getElementById('task');
 let input = document.getElementById('userinput');
 
-addtaskbutton.addEventListener('click', function (){
+addtaskbutton.addEventListener('click', function () {
    let paragraph = document.createElement('p');
-   task_el.appendChild(paragraph)
-})
+    paragraph.innerText = userinput.value;
+    task_el.appendChild(paragraph);
+})*/
 
 
 
-
-
-/*window.addEventListener('load', () => {
-   const form = document.getElementById('taskform');
+window.addEventListener('load', () => {
+   const form = document.getElementById("inputform");
    const input = document.getElementById("userinput");
    const list_el = document.getElementById("tasks");
+   const sub_btn = document.getElementById("submitbtn");
+
 form.addEventListener('submit', (e) => {
       e.preventDefault();
 
@@ -25,17 +26,48 @@ form.addEventListener('submit', (e) => {
          return;
       }
 
-      const task_el = document.createElement("div");
-      task_el.classList.add("task")
+     const task_element = document.createElement("div");
+      task_element.classList.add("taskdisplay");
 
-      const task_content_el = document.createElement("div");
-      task_content_el.classList.add("content");
-      task_content_el.innerText = task;
+      const task_content = document.createElement("div")
+      task_content.classList.add("content");
 
-      task_el.appendChild(task_content_el);
+      task_element.appendChild(task_content);
 
-      list_el.appendChild(task_el);
+      const task_input = document.createElement("input");
+      task_input.classList.add("text");
+      task_input.type = "text";
+      task_input.value = task;
+      task_input.setAttribute("readonly","readonly");
+
+      task_content.appendChild(task_input);
+
+      const task_actions =document.createElement("div");
+      task_actions.classList.add("actions");
+
+      const task_edit = document.createElement("button");
+      task_edit.classList.add("edit");
+      task_edit.innerHTML = "Edit";
+
+      const task_delete = document.createElement("button");
+      task_delete.classList.add("delete");
+      task_delete.innerHTML = "Delete";
+
+      task_actions.appendChild(task_edit);
+      task_actions.appendChild(task_delete);
+      
+
+      task_element.appendChild(task_actions);
+
+      list_el.appendChild(task_element);
+
+      input.value = "";
+      
 
    })
 
-})*/
+})
+
+/*let paragraph = document.getElementById("tasks");
+      paragraph.innerText = task;
+      list_el.appendChild(paragraph);*/
